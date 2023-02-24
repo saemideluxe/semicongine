a420a96bf8354151a49412f752f7961704eb86fd
Sam <sam@basx.dev>
2023-02-25 01:13:42 +0700

    fix: type mismatch

--- src/semicongine/platform/windows/vulkan.nim
+++ src/semicongine/platform/windows/vulkan.nim
@@ -4,7 +4,7 @@ const REQUIRED_PLATFORM_EXTENSIONS* = @["VK_KHR_win32_surface".cstring]
 proc createVulkanSurface*(instance: VkInstance, window: NativeWindow): VkSurfaceKHR =
   var surfaceCreateInfo = VkWin32SurfaceCreateInfoKHR(
     sType: VK_STRUCTURE_TYPE_WIN32_SURFACE_CREATE_INFO_KHR,
-    hinstance: window.hinstance,
-    hwnd: window.hwnd,
+    hinstance: cast[HINSTANCE](window.hinstance),
+    hwnd: cast[HWND](window.hwnd),
   )
   checkVkResult vkCreateWin32SurfaceKHR(instance, addr(surfaceCreateInfo), nil, addr(result))
