1554b9736fa9780577f7748de325426828321b7d
Sam <sam@basx.dev>
2023-03-31 16:00:16 +0700

    did: allow runtime shader-input definitions

--- src/semicongine/scene.nim
+++ src/semicongine/scene.nim
@@ -1,13 +1,15 @@
-import std/options
+import std/tables
 
 import ./vulkan/api
 import ./entity
 import ./vulkan/buffer
+import ./vulkan/pipeline
+import ./vulkan/renderpass
 
 type
   Drawable* = object
-    buffers*: seq[Buffer]
-    elementCount*: uint32
+    buffers*: seq[(Buffer, int)] # buffer + offset from buffer
+    elementCount*: uint32 # vertices or indices
     instanceCount*: uint32
     case indexed*: bool
     of true:
@@ -18,8 +20,20 @@ type
 
   Scene* = object
     root*: Entity
-    drawables: seq[Drawable]
+    drawables: Table[VkPipeline, seq[Drawable]]
 
-proc getDrawables*(scene: Scene): seq[Drawable] =
-  # TODO: create and fill buffers
-  result
+proc setupDrawables(scene: var Scene, pipeline: Pipeline) =
+  # echo pipeline.descriptorSetLayout.descriptors
+  # thetype*: VkDescriptorType
+  # count*: uint32
+  # itemsize*: uint32
+  scene.drawables[pipeline.vk] = @[]
+
+proc setupDrawables*(scene: var Scene, renderPass: var RenderPass) =
+  for subpass in renderPass.subpasses.mitems:
+    for pipeline in subpass.pipelines.mitems:
+      scene.setupDrawables(pipeline)
+
+
+proc getDrawables*(scene: Scene, pipeline: Pipeline): seq[Drawable] =
+  scene.drawables.getOrDefault(pipeline.vk, @[])
--- src/semicongine/vulkan/pipeline.nim
+++ src/semicongine/vulkan/pipeline.nim
@@ -1,11 +1,6 @@
-import std/options
-
 import ./api
-import ./utils
 import ./device
 import ./descriptor
-
-import ../scene
 
 type
   Pipeline* = object
@@ -16,38 +11,6 @@ type
     descriptorPool*: DescriptorPool
     descriptorSets*: seq[DescriptorSet]
 
-proc run*(pipeline: Pipeline, commandBuffer: VkCommandBuffer, inFlightFrame: int, scene: Scene) =
-  var varPipeline = pipeline
-  commandBuffer.vkCmdBindPipeline(VK_PIPELINE_BIND_POINT_GRAPHICS, pipeline.vk)
-  commandBuffer.vkCmdBindDescriptorSets(VK_PIPELINE_BIND_POINT_GRAPHICS, pipeline.layout, 0, 1, addr(varPipeline.descriptorSets[inFlightFrame].vk), 0, nil)
-  for drawable in scene.getDrawables():
-    var buffers: seq[VkBuffer]
-    var offsets: seq[VkDeviceSize]
-    for buffer in drawable.buffers:
-      buffers.add buffer.vk
-      offsets.add VkDeviceSize(0)
-    commandBuffer.vkCmdBindVertexBuffers(
-      firstBinding=0'u32,
-      bindingCount=uint32(buffers.len),
-      pBuffers=buffers.toCPointer(),
-      pOffsets=offsets.toCPointer()
-    )
-    if drawable.indexed:
-      commandBuffer.vkCmdBindIndexBuffer(drawable.indexBuffer.vk, VkDeviceSize(0), drawable.indexType)
-      commandBuffer.vkCmdDrawIndexed(
-        indexCount=drawable.elementCount,
-        instanceCount=drawable.instanceCount,
-        firstIndex=0,
-        vertexOffset=0,
-        firstInstance=0
-      )
-    else:
-      commandBuffer.vkCmdDraw(
-        vertexCount=drawable.elementCount,
-        instanceCount=drawable.instanceCount,
-        firstVertex=0,
-        firstInstance=0
-      )
 
 proc destroy*(pipeline: var Pipeline) =
   assert pipeline.device.vk.valid
