ebbaa9530df9243b5b605433ffc5f25bd7ab227d
Sam <sam@basx.dev>
2023-05-01 01:15:39 +0700

    add: initial implementation of audio subsystem with mixer, currently missing windows implementation

---
+++ src/semicongine/audiotypes.nim
@@ -0,0 +1,32 @@
+import std/math
+
+# in order to generate sound files that are directly usable with the engine,
+# convert an audio file to a raw PCM signed 16 bit little endian file with 2 channels and 48kHz:
+#
+# ffmpeg -i <infile> -f s16le -ac 2 -ar 48000 -acodec pcm_s16le <outfile>
+
+const SAMPLERATE* = 48000
+const BUFFERSIZE* = 512
+
+type
+  Level* = 0'f .. 1'f
+  Sample* = (int16, int16)
+  SoundData* = seq[Sample]
+  Sound* = ref SoundData
+
+proc sinewave(f: float): proc(x: float): float =
+  proc ret(x: float): float =
+    sin(x * 2 * Pi * f)
+  result = ret
+
+proc sineSoundData*(f: float, len: float): SoundData =
+  let dt = 1'f / float(SAMPLERATE)
+  var sine = sinewave(f)
+  for i in 0 ..< int(SAMPLERATE * len):
+    let t = dt * float(i)
+    let value = int16(sine(t) * float(high(int16)))
+    result.add (value, value)
+
+proc newSound*(data: SoundData): Sound =
+  result = new Sound
+  result[] = data
---
+++ src/semicongine/platform/linux/audio.nim
@@ -0,0 +1,63 @@
+import ../../audiotypes
+
+# alsa API
+type 
+  OpenMode*{.size: sizeof(culong).} = enum
+    SND_PCM_BLOCK = 0x00000000 # added by semicongine, for clarity
+    SND_PCM_NONBLOCK = 0x00000001
+  StreamMode* {.size: sizeof(cint).} = enum
+    SND_PCM_STREAM_PLAYBACK = 0
+  AccessMode*{.size: sizeof(cint).} = enum 
+    SND_PCM_ACCESS_RW_INTERLEAVED = 3
+  PCMFormat*{.size: sizeof(cint).} = enum
+    SND_PCM_FORMAT_S16_LE = 2
+  snd_pcm_p* = ptr object
+  snd_pcm_hw_params_p* = ptr object
+  snd_pcm_uframes_t* = culong
+  snd_pcm_sframes_t* = clong
+{.pragma: alsafunc, importc, cdecl, dynlib: "libasound.so" .}
+proc snd_pcm_open*(pcm_ref: ptr snd_pcm_p, name: cstring, streamMode: StreamMode, openmode: OpenMode): cint {.alsafunc.}
+proc snd_pcm_close*(pcm: snd_pcm_p): cint {.alsafunc.}
+proc snd_pcm_hw_params_malloc*(hw_params_ptr: ptr snd_pcm_hw_params_p): cint {.alsafunc.}
+proc snd_pcm_hw_params_free*(hw_params: snd_pcm_hw_params_p) {.alsafunc.}
+proc snd_pcm_hw_params_any*(pcm: snd_pcm_p, params: snd_pcm_hw_params_p): cint {.alsafunc.}
+proc snd_pcm_hw_params_set_access*(pcm: snd_pcm_p, params: snd_pcm_hw_params_p, mode: AccessMode): cint {.alsafunc.}
+proc snd_pcm_hw_params_set_format*(pcm: snd_pcm_p, params: snd_pcm_hw_params_p, format: PCMFormat): cint {.alsafunc.}
+proc snd_pcm_hw_params_set_channels*(pcm: snd_pcm_p, params: snd_pcm_hw_params_p, val: cuint): cint {.alsafunc.}
+proc snd_pcm_hw_params_set_buffer_size*(pcm: snd_pcm_p, params: snd_pcm_hw_params_p, size: snd_pcm_uframes_t): cint {.alsafunc.}
+proc snd_pcm_hw_params_set_rate*(pcm: snd_pcm_p, params: snd_pcm_hw_params_p, val: cuint, dir: cint): cint {.alsafunc.}
+proc snd_pcm_hw_params*(pcm: snd_pcm_p, params: snd_pcm_hw_params_p): cint {.alsafunc.}
+proc snd_pcm_writei*(pcm: snd_pcm_p, buffer: pointer, size: snd_pcm_uframes_t): snd_pcm_sframes_t {.alsafunc.}
+
+template checkAlsaResult*(call: untyped) =
+  let value = call
+  if value < 0:
+    raise newException(Exception, "Alsa error: " & astToStr(call) &
+      " returned " & $value)
+
+# required for engine:
+
+type
+  NativeSoundDevice* = object
+    handle: snd_pcm_p
+ 
+proc openSoundDevice*(sampleRate: uint32, bufferSize: uint32): NativeSoundDevice =
+  var hw_params: snd_pcm_hw_params_p = nil
+  checkAlsaResult snd_pcm_open(addr result.handle, "default", SND_PCM_STREAM_PLAYBACK, SND_PCM_BLOCK)
+
+  # hw parameters, quiet a bit of hardcoding here
+  checkAlsaResult snd_pcm_hw_params_malloc(addr hw_params)
+  checkAlsaResult snd_pcm_hw_params_any(result.handle, hw_params)
+  checkAlsaResult snd_pcm_hw_params_set_access(result.handle, hw_params, SND_PCM_ACCESS_RW_INTERLEAVED)
+  checkAlsaResult snd_pcm_hw_params_set_format(result.handle, hw_params, SND_PCM_FORMAT_S16_LE)
+  checkAlsaResult snd_pcm_hw_params_set_rate(result.handle, hw_params, sampleRate, 0)
+  checkAlsaResult snd_pcm_hw_params_set_channels(result.handle, hw_params, 2)
+  checkAlsaResult snd_pcm_hw_params_set_buffer_size(result.handle, hw_params, snd_pcm_uframes_t(bufferSize))
+  checkAlsaResult snd_pcm_hw_params(result.handle, hw_params)
+  snd_pcm_hw_params_free(hw_params)
+
+proc updateSoundBuffer*(soundDevice: NativeSoundDevice, buffer: var SoundData) =
+  discard snd_pcm_writei(soundDevice.handle, addr buffer[0], snd_pcm_uframes_t(len(buffer)))
+
+proc closeSoundDevice*(soundDevice: NativeSoundDevice) =
+  checkAlsaResult snd_pcm_close(soundDevice.handle)
