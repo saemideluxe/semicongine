faf1f9481625d9612fb7f7939c10e8fd11d888d7
Sam <sam@basx.dev>
2023-05-06 23:06:33 +0700

    add: textures now support in shader via scene data, also: improved config handling a bit, more to come

--- src/semicongine.nim
+++ src/semicongine.nim
@@ -1,6 +1,8 @@
+import semicongine/buildconfig
+export buildconfig
+
 import semicongine/audio
 import semicongine/color
-import semicongine/config
 import semicongine/engine
 import semicongine/entity
 import semicongine/events
@@ -8,12 +10,12 @@ import semicongine/gpu_data
 import semicongine/math
 import semicongine/mesh
 import semicongine/renderer
+import semicongine/settings
 import semicongine/platform/window
 import semicongine/vulkan
 
 export audio
 export color
-export config
 export engine
 export entity
 export events
@@ -21,5 +23,6 @@ export gpu_data
 export math
 export mesh
 export renderer
+export settings
 export window
 export vulkan
