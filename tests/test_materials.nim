1da6aae6bbf27ebb638ef3767aa74301e23f9fce
Sam <sam@basx.dev>
2024-01-29 00:19:35 +0700

    fix: all tests (once more)

--- tests/test_materials.nim
+++ tests/test_materials.nim
@@ -6,16 +6,16 @@ import semicongine
 let
   sampler = Sampler(magnification: VK_FILTER_NEAREST, minification: VK_FILTER_NEAREST)
   (RT, WT, PT) = (toRGBA("A51931").asPixel, toRGBA("F4F5F8").asPixel, toRGBA("2D2A4A").asPixel)
-  thai = Image(width: 7, height: 5, imagedata: @[
+  thai = Image[RGBAPixel](width: 7, height: 5, imagedata: @[
     RT, RT, RT, RT, RT, RT, RT,
     WT, WT, WT, WT, WT, WT, WT,
     PT, PT, PT, PT, PT, PT, PT,
     WT, WT, WT, WT, WT, WT, WT,
     RT, RT, RT, RT, RT, RT, RT,
   ])
-  swiss = loadImage("flag.png")
+  swiss = loadImage[RGBAPixel]("flag.png")
   doubleTextureMaterial = MaterialType(
-    name:"Double texture",
+    name: "Double texture",
     vertexAttributes: {
       "position": Vec3F32,
       "uv": Vec2F32,
@@ -23,11 +23,11 @@ let
     attributes: {"tex1": TextureType, "tex2": TextureType}.toTable
   )
   material = initMaterialData(
-    theType=doubleTextureMaterial,
-    name="swiss-thai",
-    attributes={
-      "tex1": initDataList(@[Texture(image: thai, sampler: sampler)]),
-      "tex2": initDataList(@[Texture(image: swiss, sampler: sampler)]),
+    theType = doubleTextureMaterial,
+    name = "swiss-thai",
+    attributes = {
+      "tex1": initDataList(@[Texture(colorImage: thai, sampler: sampler, isGrayscale: false)]),
+      "tex2": initDataList(@[Texture(colorImage: swiss, sampler: sampler, isGrayscale: false)]),
     }
   )
 
@@ -41,23 +41,23 @@ proc main() =
 
   const
     shaderConfiguration1 = createShaderConfiguration(
-      inputs=[
-        attr[Vec3f]("position", memoryPerformanceHint=PreferFastRead),
-        attr[Vec2f]("uv", memoryPerformanceHint=PreferFastRead),
+      inputs = [
+        attr[Vec3f]("position", memoryPerformanceHint = PreferFastRead),
+        attr[Vec2f]("uv", memoryPerformanceHint = PreferFastRead),
       ],
-      intermediates=[
+      intermediates = [
         attr[Vec2f]("uvout"),
       ],
-      uniforms=[attr[float32]("test2", arrayCount=2)],
+      uniforms = [attr[float32]("test2", arrayCount = 2)],
       samplers = @[
-        attr[Texture]("tex1", arrayCount=1),
-        attr[Texture]("tex2", arrayCount=1),
+        attr[Texture]("tex1", arrayCount = 1),
+        attr[Texture]("tex2", arrayCount = 1),
       ],
-      outputs=[attr[Vec4f]("color")],
-      vertexCode="""
+      outputs = [attr[Vec4f]("color")],
+      vertexCode = """
       gl_Position = vec4(position.x, position.y + sin(Uniforms.test2[1]) / Uniforms.test2[1] * 0.5, position.z, 1.0);
       uvout = uv;""",
-      fragmentCode="""
+      fragmentCode = """
       float d = sin(Uniforms.test2[0]) * 0.5 + 0.5;
       color = texture(tex1[0], uvout) * (1 - d) + texture(tex2[0], uvout) * d;
       """,
