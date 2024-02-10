753c2f6c39f52a35256781c6740e54516fe4074b
Sam <sam@basx.dev>
2024-02-10 15:55:05 +0700

    add: initial implementation of "panels"

--- semicongine/panel.nim
+++ semicongine/panel.nim
@@ -1,11 +1,43 @@
 import std/strformat
+import std/tables
 
 import ./core
 import ./mesh
+import ./material
+import ./vulkan/shader
 
 const
+  # font shader
   SHADER_ATTRIB_PREFIX = "semicon_panel_"
   MAX_PANEL_MATERIALS = 10
+  POSITION_ATTRIB = SHADER_ATTRIB_PREFIX & "position"
+  UV_ATTRIB = SHADER_ATTRIB_PREFIX & "uv"
+  PANEL_MATERIAL_TYPE* = MaterialType(
+    name: "default-panel-material-type",
+    vertexAttributes: {TRANSFORM_ATTRIB: Mat4F32, POSITION_ATTRIB: Vec3F32, UV_ATTRIB: Vec2F32}.toTable,
+    attributes: {"panelTexture": TextureType, "color": Vec4F32}.toTable,
+  )
+  PANEL_SHADER* = createShaderConfiguration(
+    inputs = [
+      attr[Mat4](TRANSFORM_ATTRIB, memoryPerformanceHint = PreferFastWrite, perInstance = true),
+      attr[Vec3f](POSITION_ATTRIB, memoryPerformanceHint = PreferFastWrite),
+      attr[Vec2f](UV_ATTRIB, memoryPerformanceHint = PreferFastWrite),
+      attr[uint16](MATERIALINDEX_ATTRIBUTE, memoryPerformanceHint = PreferFastRead, perInstance = true),
+    ],
+    intermediates = [
+      attr[Vec2f]("uvFrag"),
+      attr[uint16]("materialIndexOut", noInterpolation = true)
+    ],
+    outputs = [attr[Vec4f]("color")],
+    uniforms = [attr[Vec4f]("color", arrayCount = MAX_PANEL_MATERIALS)],
+    samplers = [attr[Texture]("panelTexture", arrayCount = MAX_PANEL_MATERIALS)],
+    vertexCode = &"""
+  gl_Position = vec4({POSITION_ATTRIB}, 1.0) * {TRANSFORM_ATTRIB};
+  uvFrag = {UV_ATTRIB};
+  materialIndexOut = {MATERIALINDEX_ATTRIBUTE};
+  """,
+    fragmentCode = &"""color = Uniforms.color[materialIndexOut] * texture(panelTexture[materialIndexOut], uvFrag);"""
+  )
 
 var instanceCounter = 0
 
@@ -13,7 +45,6 @@ type
   Panel* = object
     position: Vec2f
     size: Vec2f
-    color*: Vec4f
 
     texture: Texture
     horizontalAlignment: HorizontalAlignment = Center
@@ -22,19 +53,63 @@ type
     dirty: bool
     mesh: Mesh
 
+proc `$`*(panel: Panel): string =
+  &"Panel {panel.position} (size {panel.size})"
+
+proc refresh*(panel: var Panel) =
+  if not panel.dirty:
+    return
+
+  var
+    offsetX = case panel.horizontalAlignment
+      of Left: panel.size.x / 2
+      of Center: 0
+      of Right: -panel.size.x / 2
+    offsetY = case panel.verticalAlignment
+      of Top: panel.size.y / 2
+      of Center: 0
+      of Bottom: -panel.size.y / 2
+
+  panel.mesh[POSITION_ATTRIB, 0] = newVec3f(
+    panel.position.x - panel.size.x / 2 + offsetX,
+    (panel.position.y - panel.size.y / 2 + offsetY) * panel.aspect_ratio
+  )
+  panel.mesh[POSITION_ATTRIB, 1] = newVec3f(
+    panel.position.x + panel.size.x / 2 + offsetX,
+    (panel.position.y - panel.size.y / 2 + offsetY) * panel.aspect_ratio
+  )
+  panel.mesh[POSITION_ATTRIB, 2] = newVec3f(
+    panel.position.x + panel.size.x / 2 + offsetX,
+    (panel.position.y + panel.size.y / 2 + offsetY) * panel.aspect_ratio
+  )
+  panel.mesh[POSITION_ATTRIB, 3] = newVec3f(
+    panel.position.x - panel.size.x / 2 + offsetX,
+    (panel.position.y + panel.size.y / 2 + offsetY) * panel.aspect_ratio
+  )
+
+  panel.dirty = false
+
 proc initPanel*(position = newVec2f(), size = newVec2f(), color = newVec4f(1, 1, 1, 1), texture = EMPTY_TEXTURE, horizontalAlignment = HorizontalAlignment.Center, verticalAlignment = VerticalAlignment.Center): Panel =
 
-  result = Panel(position: position, size: size, color: color, texture: texture, horizontalAlignment: horizontalAlignment, verticalAlignment: verticalAlignment, aspect_ratio: 1)
+  result = Panel(position: position, size: size, texture: texture, horizontalAlignment: horizontalAlignment, verticalAlignment: verticalAlignment, aspect_ratio: 1)
 
-  inc instanceCounter
-  var
-    positions = newSeq[Vec3f](4)
+  result.mesh = newMesh(
+    positions = newSeq[Vec3f](4),
     indices = @[
       [uint16(0), uint16(1), uint16(2)],
       [uint16(2), uint16(3), uint16(0)],
-    ]
-    uvs = newSeq[Vec2f](4)
-  result.mesh = newMesh(positions = positions, indices = indices, uvs = uvs, name = &"panel-{instanceCounter}")
+    ],
+    uvs = @[newVec2f(0, 1), newVec2f(1, 1), newVec2f(1, 0), newVec2f(0, 0)], name = &"panel-{instanceCounter}"
+  )
+  result.mesh[].renameAttribute("position", POSITION_ATTRIB)
+  result.mesh[].renameAttribute("uv", UV_ATTRIB)
+  result.mesh.material = initMaterialData(
+    theType = PANEL_MATERIAL_TYPE,
+    name = "Panel material",
+    attributes = {"panelTexture": initDataList(@[texture]), "color": initDataList(@[color])},
+  )
+  inc instanceCounter
+  result.refresh()
 
 proc position*(panel: Panel): Vec2f =
   panel.position
@@ -42,6 +117,12 @@ proc `position=`*(panel: var Panel, value: Vec2f) =
   if value != panel.position:
     panel.position = value
     panel.dirty = true
+
+proc color*(panel: Panel): Vec4f =
+  panel.mesh.material["color", 0, Vec4f]
+proc `color=`*(panel: var Panel, value: Vec4f) =
+  if value != panel.color:
+    panel.mesh.material["color", 0] = value
 
 proc size*(panel: Panel): Vec2f =
   panel.size
