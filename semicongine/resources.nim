9ad9d0cff61e79fcff5024659e1fe2bc4276efab
sam <sam@basx.dev>
2024-03-25 07:52:24 -0700

    fix: windows paths

--- semicongine/resources.nim
+++ semicongine/resources.nim
@@ -132,7 +132,7 @@ elif thebundletype == Exe:
         let package = packageDir.splitPath.tail
         result[package] = Table[string, string]()
         for resourcefile in walkDirRec(packageDir, relative = true):
-          result[package][resourcefile.normalizeDir] = staticRead(packageDir.joinPath(resourcefile))
+          result[package][resourcefile.replace('\\', '/')] = staticRead(packageDir.joinPath(resourcefile))
   const bundledResources = loadResources()
 
   proc loadResource_intern(path: string, package: string): Stream =
