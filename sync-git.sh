3b4b0056ad5d88d9c9aec62fe708bac9c2b6de70
sam <sam@basx.dev>
2024-07-28 23:14:22 +0700

    fix: git-sync script

--- sync-git.sh
+++ sync-git.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-rsync -r --exclude '.hg/' --excdlue 'build/' . /home/sam/git/semicongine/
+rsync -r --exclude '.hg/' --exclude 'build/' --exclude 'sync-git.sh' . /home/sam/git/semicongine/
 
 git -C /home/sam/git/semicongine/ commit -a -m $( hg log --limit 1 --template "{desc}" )
